f6d8c438a7a126ced3c8671167cbbed3ccf0614e
Adammm <adambahymaulana7@gmail.com>
2024-10-31 08:25:14 +0700

    push modifikasi ArrayRataNilai01

--- ArrayRataNilai01.java
+++ ArrayRataNilai01.java
@@ -1,11 +1,14 @@
 import java.util.*;
+
 public class ArrayRataNilai01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        int jumlah = 0;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        jumlah = sc.nextInt();
+        int[] nilaiMhs = new int[jumlah];
+        double lulus = 0, tidaklulus = 0;
+        double nilailulus = 0, nilaitidaklulus = 0, ratalulus = 0, ratatidaklulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
@@ -13,10 +16,19 @@ public class ArrayRataNilai01 {
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                nilailulus += nilaiMhs[i];
+                lulus++;
+            } else {
+                nilaitidaklulus += nilaiMhs[i];
+                tidaklulus++;
+            }
         }
 
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        ratalulus = nilailulus / lulus;
+        ratatidaklulus = nilaitidaklulus / tidaklulus;
+
+        System.out.println("Rata-rata lulus = " + ratalulus);
+        System.out.println("Rata-rata lulus = " + ratatidaklulus);
     }
 }
