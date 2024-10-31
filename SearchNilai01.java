2dde15bf1fe14b3d0f437c760478d298c4147d38
Adammm <adambahymaulana7@gmail.com>
2024-10-31 08:59:12 +0700

    push modifikasi SearchNilai01

--- SearchNilai01.java
+++ SearchNilai01.java
@@ -1,7 +1,24 @@
+import java.util.*;
+
 public class SearchNilai01 {
     public static void main(String[] args) {
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
+
+        Scanner sc = new Scanner(System.in);
+
+        int jml = 0;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        jml = sc.nextInt();
+
+        int[] arrNilai = new int[jml];
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        int key = 0;
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        key = sc.nextInt();
+
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
@@ -11,8 +28,14 @@ public class SearchNilai01 {
             }
         }
 
-        System.out.println();
-        System.out.println("Nilai " + key + " Ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil != 0) {
+            System.out.println();
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+            System.out.println();
+        } else {
+            System.out.println();
+            System.out.println("Nilai yang dicari tidak ditemukan");
+            System.out.println();
+        }
     }
 }
